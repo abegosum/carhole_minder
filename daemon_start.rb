62bf924aad440d71610a9960547a9a5b8960884b
Aaron M. Bond <ambond@aaronmbond.com>
2018-05-30 02:09:51 +0000

    Updated with drb service

--- daemon_start.rb
+++ daemon_start.rb
@@ -1,4 +1,10 @@
 
 require_relative 'carhole_minder'
+require_relative 'constants'
+require 'drb/drb'
+
 service = CarholeMinder.new
+
+DRb.start_service("druby://localhost:#{DRB_PORT}", service)
+
 service.run!
