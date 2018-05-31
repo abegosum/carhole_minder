4f4306ab265420ba6b394ef0d63f132c93c5789b
Aaron M. Bond <ambond@aaronmbond.com>
2018-05-31 05:42:57 +0000

    Got to work with first safe value!

--- daemon_start.rb
+++ daemon_start.rb
@@ -4,13 +4,13 @@ require_relative 'service_frontend'
 require_relative 'constants'
 require 'drb/drb'
 
-SERVICE_SAFE = 3
+SERVICE_SAFE = 1
 
 daemon_object = CarholeMinder.new
 
 drb_front_object = ServiceFrontend.new(daemon_object)
 
-#DRb.start_service("druby://localhost:#{DRB_PORT}", drb_front_object, SERVICE_SAFE)
-DRb.start_service("druby://localhost:#{DRB_PORT}", drb_front_object)
+DRb.start_service("druby://localhost:#{DRB_PORT}", drb_front_object, { :safe_level => SERVICE_SAFE})
+#DRb.start_service("druby://localhost:#{DRB_PORT}", drb_front_object)
 
 daemon_object.run!
